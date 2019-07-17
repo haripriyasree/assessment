38fe9ad5527671e7385765774a55bd3b87bc468a
haripriyasree <haripriya.s@hebbalelabs.com>
2019-07-17 18:21:08 +0530

    removed tags

--- src/main/java/com/talfinder/assessment/RedundantBracesCheck.java
+++ src/main/java/com/talfinder/assessment/RedundantBracesCheck.java
@@ -8,41 +8,35 @@ public class RedundantBracesCheck {
         Stack<Character> st = new Stack<Character>();
         boolean flag=true;
         // char[] str = exp.toCharArray();
-        // Iterate through the given expression
+
         char[] ch=exp.toCharArray();
         for (int i=0;i<exp.length();i++) {
 
-            // if current character is close parenthesis ')'
             if (exp.charAt(i) == '}'||exp.charAt(i)==')'||exp.charAt(i)==']') {
 
                 st.pop();
 
-                // If immediate pop have open parenthesis '('
-                // duplicate brackets found
 
                 if (st.peek() == '{'||st.peek()=='('||st.peek()=='[')
                     flag= true;
                 else {
                     if (st.peek() != '{'|| st.peek()!='('||st.peek()!='[') {
 
-                        // Check for operators in expression
                         if (st.peek() == '+' || st.peek() == '-'
                                 || st.peek() == '*' || st.peek() == '/') {
                             flag = false;
                         }
 
-                        // Fetch top element of stack
 
                         st.pop();
                     }
                 }
-                // If operators not found
                 if (flag == true) {
                     return true;
                 }
             } else {
-                st.push(exp.charAt(i)); // push open parenthesis '(',
-            }                // operators and operands to stack
+                st.push(exp.charAt(i));
+            }                
         }
         return false;
 
